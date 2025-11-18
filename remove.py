1b95bd8daba71c55e1133fc30cb760ba6132ac41
loo <loofxtrt@gmail.com>
2025-11-17 21:25:59 -0300

    etc

---
+++ remove.py
@@ -0,0 +1,11 @@
+from pathlib import Path
+
+def remove(icon_pack_root: Path):
+    to_be_removed = [
+        'places/scalable/folder-3dprint',
+        'folder-go'
+    ]
+
+    for tbr in to_be_removed:
+        tbr += '.svg'
+        path = icon_pack_root / tbr
